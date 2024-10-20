3146a77d79d1b9e98175f73ab72de32d5bef23cf
Dallefar <94932668+Dallefar@users.noreply.github.com>
2024-10-20 14:04:18 +0200

    Files del

--- config.lua
+++ config.lua
@@ -1,16 +1,22 @@
 Config = {
-  Jobs = {
-    "police",
-    "ambulance"
-  },
+
   TakeCall = "Dit opkald blev taget",
+  
   HotKey = "F7",
+
   AutomaticMessage = "Anonymt",
+
   SendTakenMessage = true,
+  
+  Jobs = {
+    "Politi-Job",
+    "Mekaniker"
+  },
+
   Sounds = {
     "sound1.ogg",
     "sound2.ogg",
-	"sound3.ogg",
-	"sound4.ogg"
+    "sound3.ogg",
+    "sound4.ogg"
   }
 }
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,25 +6,22 @@ description 'Calllist made by Visualz Development'
 author 'Visualz Development <support@visualz.dk>'
 version '1.0.0'
 
-
 ui_page 'dist/index.html'
 file 'dist/**/*'
 
-client_script 'client/**/*'
+client_scripts {
+    "lib/Tunnel.lua",
+    "lib/Proxy.lua",
+	'client/**/*'
+}
 
 server_scripts {
+	'@vrp/lib/utils.lua',
 	'server/**/*',
 	'@oxmysql/lib/MySQL.lua',
 }
 
 shared_scripts {
 	'config.lua',
-	'@es_extended/imports.lua',
 	'@ox_lib/init.lua',
-}
-
-escrow_ignore {
-	'server/exports.lua',
-	'client/exports.lua',
-	'config.lua'
 }
